8fc58a1aefd8bb46abb3a4178a1ec54a4f460e73
kevin0712 <108166303+kevin970712@users.noreply.github.com>
2026-07-16 21:38:34 +0800

    v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.android.sttranslate"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/android/sttranslate/MainActivity.kt
+++ app/src/main/java/com/android/sttranslate/MainActivity.kt
@@ -49,6 +49,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
@@ -202,6 +203,7 @@ fun InputArea(
                     bottom = UIConfig.VerticalPadding
                 ),
             textStyle = MaterialTheme.typography.headlineSmall.copy(color = MaterialTheme.colorScheme.onSurface),
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
             keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
             keyboardActions = KeyboardActions(onDone = { onTranslate() }),
             decorationBox = { innerTextField ->
